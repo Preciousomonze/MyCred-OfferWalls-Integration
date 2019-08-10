1bf361bbbe0aadeb82989f845b3e0f3baac437f6
Precious Omonzejele <crispusomonze@gmail.com>
2019-08-10 11:14:56 +0100

    unfixed:sorting out wpbakery exec shortcodes :'(

--- inc/class-shortcodes.php
+++ inc/class-shortcodes.php
@@ -5,6 +5,8 @@ if ( ! defined( 'ABSPATH' ) ) {
     class PK_MC_OW_Shortcodes {
         public function __construct() {
             add_action('init', function(){
+                add_action( 'rest_api_init', array($this, 'rest_api'));
+                //add_filter('the_content', 'do_shortcode');//for wp_bakery to run shortcodes
                 add_shortcode( PK_MC_OW_USERNAME_SHORTCODE, array( $this, 'current_username' ) );  
             });
         }
@@ -21,6 +23,39 @@ if ( ! defined( 'ABSPATH' ) ) {
             $user = get_user_by('id',get_current_user_id());
             if($user){
                 return $user->user_login;
+            }
+            return $result;
+        }
+        /**
+        * Modify REST API content for pages to force
+        * shortcodes to render since Visual Composer does not do this :)
+        * Thanks to some github guys @https://github.com/WP-API/WP-API/issues/2578
+        */
+        public function rest_api(){
+            register_rest_field(
+            'page',
+            'content',
+            array(
+                 'get_callback'    => array($this,'do_shortcodes'),
+                 'update_callback' => null,
+                 'schema'          => null,
+            )
+            );
+        }
+
+        /**
+         * Callback function for the api
+         */
+        public function do_shortcodes( $object, $field_name, $request ){
+            global $post;
+            $post = get_post ($object['id']);
+            if(class_exists('WPBMap')){
+                WPBMap::addAllMappedShortcodes(); // This does all the work
+                $output['rendered'] = apply_filters( 'the_content', $post->post_content );
+                return $output;
+            }
+            else{// do nothing
+                return apply_filters( 'the_content', $post->post_content );
             }
         }
     }
